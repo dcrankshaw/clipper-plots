c2dcc3ccf8771daf98b04779efdf869a3bcac811
Dan Crankshaw <dscrankshaw@gmail.com>
2016-09-15 23:49:51 -0700

    moved plots to python and added comments

---
+++ nsdi/plotting/batching_scatter_plots.py
@@ -0,0 +1,128 @@
+from __future__ import print_function
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import sys
+import seaborn as sns
+import statsmodels.formula.api as smf
+import utils
+
+"""
+NOTES:
+    For this experiment, we ran the adaptive batching search
+    algorithm from its initialization with batch size 1 for
+    1000 samples for each model with a latency objective of
+    20ms. The scatter plots show the batch size and measured
+    latency of each sample. We then fit a 99-percentile quantile
+    regression model and plot this as well. The AIMD Decrease points
+    are the samples where the adaptive algorithm decreased the
+    maximum batch size, and we can see that these are exactly
+    the points above the latency objective.
+
+"""
+
+
+fig_dir = utils.NSDI_FIG_DIR
+
+
+# sns.set_style("darkgrid")
+sns.set_style("darkgrid")
+sns.set_context("paper", font_scale=0.75,)
+
+
+def find_backtrack_points(batches, lats, k=False):
+    bt_batches = []
+    bt_lats = []
+    idx = []
+    for i in range(len(batches) - 1):
+        if batches[i] > batches[i + 1]:
+            if k:
+                bt_batches.append(batches[i])
+                bt_lats.append(lats[i])
+                idx.append(i)
+            else:
+                if batches[i] > 50:
+                    bt_batches.append(batches[i])
+                    bt_lats.append(lats[i])
+                    idx.append(i)
+    print(idx)
+    return (bt_batches, bt_lats)
+
+def process_batch(results, name, ax, title=None):
+    splits = [(r["batch_size"], r["latency"]) for r in results[name]["measurements"]]
+    batches, latencies = zip(*splits)
+    df = pd.DataFrame({"batch_size": batches, "latencies": latencies})
+    
+    # Fit the quantile regression line
+    quantiles = [0.99]
+    mod = smf.quantreg('latencies ~ batch_size', df)
+    def fit_model(q):
+        model_fit = mod.fit(q=q)
+        return [q, model_fit.params['Intercept'], model_fit.params['batch_size']] + model_fit.conf_int().ix['batch_size'].tolist()    
+    models = [fit_model(cur_q) for cur_q in quantiles]
+    models = pd.DataFrame(models, columns=['q', 'a', 'b','lb','ub']) 
+    x_max_lim = np.max(batches)*1.2
+    x = np.arange(0, x_max_lim, 1)
+    # colors = sns.cubehelix_palette(4, start=2.8, rot=-0.1)
+    colors=sns.color_palette("cubehelix", 5)
+    bt_batches, bt_lats = find_backtrack_points(batches, latencies, name=="kernel_svm")
+    lw=1
+    ax.scatter(batches, latencies, color=colors[0], s=7, label="Samples")
+    ax.scatter(bt_batches, bt_lats, color=colors[3], s=25, label="AIMD Decrease")
+    get_y = lambda a, b: a + b * x
+    for i in range(models.shape[0]):
+        y = get_y(models.a[i], models.b[i])
+        ax.plot(x, y, linestyle='-', color=colors[1], linewidth=lw, label="P%d Regression Line" % int(models.q[i]*100))
+    ax.plot([0, x_max_lim], np.ones(2)*20000, color=colors[2], linestyle="dashed", linewidth=lw, label = "SLO")
+    ax.set_xlim((0, x_max_lim))
+    ax.set_ylim((0, max(np.max(latencies)*1.3, 26000)))   
+
+
+if __name__=="__main__":
+    results = {}
+    raw_results_fname = os.path.abspath("../results/batching_scatter_plots_raw_logs.txt")
+
+
+
+
+    with open(raw_results_fname, "r") as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            line = lines[i]
+            if "GREPTHIS" in line:
+                splits = line.strip().split("XXXXXX")
+                res = json.loads(splits[1].strip())
+                results[res["name"]] = res
+
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(8, 3))
+    axs = axs.flatten()
+    for (i,n) in enumerate(results.keys()):
+        ax = axs[i]
+        # print(ax)
+        process_batch(results, n, ax)
+        title = "%s) %s" % (chr(i + ord('a')), utils.name_map[n])
+
+        ax.text(0.5, 0.9, title, fontdict={"weight": "bold",},
+                horizontalalignment='center',
+                verticalalignment='center',
+                transform=ax.transAxes,
+                # backgroundcolor="white"
+                )
+
+    legend = axs[1].legend(frameon=True, bbox_to_anchor=(-1.2, 1.02, 3.4, .102), loc=3,
+            ncol=4, mode="expand", borderaxespad=0.1, fontsize=8,)
+
+
+    # if title is None:
+    axs[0].set_ylabel("Latency ($\mu$s)")
+    axs[3].set_ylabel("Latency ($\mu$s)")
+    axs[3].set_xlabel("Batch Size")
+    axs[4].set_xlabel("Batch Size")
+    axs[5].set_xlabel("Batch Size")
+    fig.subplots_adjust(hspace=0.2)
+
+    fname = "%s/batching_scatterplots.pdf" % (fig_dir)
+    plt.savefig(fname, bbox_inches='tight')
+    print(fname)
---
+++ nsdi/plotting/feedback_caching.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import sys
+import seaborn as sns
+import utils
+sns.set_style("white")
+sns.set_context("paper", font_scale=1.2)
+
+fig_dir = utils.NSDI_FIG_DIR
+# fig_dir = "test/"
+
+log_loc = os.path.abspath("../results/feedback_caching")
+
+def get_results(fname):
+    res_fname = fname + "_results.json"
+    conf_fname = fname + "_config.json"
+    with open(os.path.join(log_loc, res_fname), "r") as rf, open(os.path.join(log_loc, conf_fname), "r") as cf:
+        results = json.load(rf)
+        conf = json.load(cf)
+        cache_off = conf["clipper_conf"]["salt_cache"]
+        window_size = conf["clipper_conf"]["window_size"]
+        update_cache_rate = [rc["ratio"] for rc in results["ratio_counters"] if rc["name"] == "update_cache_hits"][0]
+        update_thruput = [m["rate"] for m in results["meters"] if m["name"] == "update_thruput"][0]
+    return (cache_off, window_size, update_cache_rate, update_thruput)
+
+
+results_files = []
+for name in os.listdir(log_loc):
+    if "results" in name:
+        results_files.append(name)
+
+exp_names = [f.split("_results")[0] for f in results_files]
+
+
+extracted_results = [get_results(e) for e in exp_names]
+
+
+extracted_results.sort()
+cache_state, windows, hit_rate, cache_on_thrus = zip(*extracted_results[:3])
+cache_state, windows, hit_rate, cache_off_thrus = zip(*extracted_results[3:])
+colors = sns.cubehelix_palette(2, start=2.8, rot=-0.1)
+fig, ax = plt.subplots(figsize=(5,2.5))
+sns.despine()
+width = 1
+def autolabel(rects):
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+r1 = ax.bar(np.arange(len(cache_off_thrus))*width*3,  cache_on_thrus, width=width, color=colors[0], label="caching")
+r2 = ax.bar(np.arange(len(cache_off_thrus))*width*3 + 1,  cache_off_thrus, width=width, color=colors[1], label="no caching")
+autolabel(r1)
+autolabel(r2)
+
+plt.xticks(np.arange(len(cache_off_thrus))*width*3 + 1, windows)
+ax.legend(loc=0)
+ax.set_xlabel("Update Window Size")
+ax.set_ylabel("Update Throughput")
+fname = "%s/caching_for_feedback_thruput.pdf" % (fig_dir)
+plt.savefig(fname, bbox_inches='tight')
+print(fname)
