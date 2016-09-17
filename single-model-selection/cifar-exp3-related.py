5bbad45db55bfe3fb96c55c693ae9a381480081d
xinw1012 <xinw1012@gmail.com>
2016-09-17 14:19:04 -0700

    update robust prediction & single model selection code

--- single-model-selection/cifar-exp3-related.py
+++ single-model-selection/cifar-exp3-related.py
@@ -1,34 +1,40 @@
 import numpy as np
 import json
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-
+sns.set(style="white")
 ### cifar10 exp3 for single model selection
 original_acc = [0.7575, 0.654, 0.832, 0.54, 0.9085]
-avg_ac = json.load(open('../data/cifar-exp3-acc.json', 'r'))
-fig, ax = plt.subplots()
-ax.plot(range(len(avg_ac)), avg_ac, label='exp3')
+avg_ac = json.load(open('./data/cifar-exp3-acc.json', 'r'))
+fig, ax = plt.subplots(figsize=(4,3))
+colors = sns.color_palette("bright", n_colors=6, desat=.5)
+ax.plot(range(len(avg_ac)), avg_ac, color=colors[0],label='exp3')
 for i in range(5):
-    ax.plot(range(len(avg_ac)), np.ones((len(avg_ac),))*original_acc[i], '--',label='model'+str(i+1))
-plt.xlabel('queries')
-plt.ylabel('accuracy')
+    ax.plot(range(len(avg_ac)), np.ones((len(avg_ac),))*original_acc[i], '--', color=colors[i+1],label='model'+str(i+1))
+plt.xlabel('queries', fontsize=15)
+plt.ylabel('accuracy', fontsize=15)
+ax.tick_params(axis='both', which='major', labelsize=13)
+ax.tick_params(axis='both', which='minor', labelsize=10)
 ax.set_ylim([0.5,1])
-ax.legend(loc=4)
-plt.savefig('cifar10-exp3.pdf')
+ax.legend(loc=4, frameon=True)
+plt.savefig('cifar10-exp3.pdf', bbox_inches='tight')
 
 ### failure in model5 
 original_acc = [0.7575, 0.654, 0.832, 0.54, 0.9085]
-avg_drift = json.load(open('../data/cifar-exp3-drift.json', 'r'))
-fig, ax = plt.subplots()
-ax.plot(range(len(avg_drift)), avg_drift, label='exp3-drift')
+avg_drift = json.load(open('./data/cifar-exp3-drift.json', 'r'))
+fig, ax = plt.subplots(figsize=(4,3))
+ax.plot(range(len(avg_drift)), avg_drift, color=colors[0],label='exp3-drift')
 for i in range(5):
-    ax.plot(range(len(avg_drift)), np.ones((len(avg_drift),))*original_acc[i], '--',label='model'+str(i+1))
+    ax.plot(range(len(avg_drift)), np.ones((len(avg_drift),))*original_acc[i], '--', color=colors[i+1],label='model'+str(i+1))
 plt.axvline(x=1000,color='black',linestyle='--')
 ax.annotate('failure in model5', xy=(1001, 0.9), xytext=(1300, 0.92),
-            arrowprops=dict(facecolor='black', shrink=0.05),
+            arrowprops=dict(facecolor='black', shrink=0.05), size=13
             )
-plt.xlabel('queries')
-plt.ylabel('accuracy')
+plt.xlabel('queries', fontsize=15)
+plt.ylabel('accuracy', fontsize=15)
+ax.tick_params(axis='both', which='major', labelsize=13)
+ax.tick_params(axis='both', which='minor', labelsize=10)
 ax.set_ylim([0.5,1])
-ax.legend(loc=4)
-plt.savefig('cifar10-exp3-drift.pdf')
+ax.legend(loc=4, frameon=True)
+plt.savefig('cifar10-exp3-drift.pdf', bbox_inches='tight')
