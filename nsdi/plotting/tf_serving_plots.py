3426090850da718849529b9e08e8c236314761d3
Giulio Zhou <giuliozhou8@gmail.com>
2016-09-19 22:15:00 -0700

    Combined latency/throughput plots with p99s

--- nsdi/plotting/tf_serving_plots.py
+++ nsdi/plotting/tf_serving_plots.py
@@ -23,49 +23,66 @@ name_map = {
         }
 
 def plot_thrus_latency(figsize, colors):
-    fix, ax = plt.subplots(figsize=figsize, nrows=3, ncols=2)
+    fig, big_axes = plt.subplots(figsize=figsize , nrows=3, ncols=1)
+    # fix, ax = plt.subplots(figsize=figsize, nrows=3, ncols=2)
     sns.despine()
     width = 1.0
     space = 0.7
     num_bars = 3
     thruput = {
-        "MNIST": {"tf_serving": 22795.805, "cpp_rpc": 21390.58408, "python": 19869.90862},
-        "CIFAR-10": {"tf_serving": 5228, "cpp_rpc": 5346.845183, "python": 4461.848557},
-        "ImageNet": {"tf_serving": 57.35, "cpp_rpc": 53.71576, "python": 46.99235537}
+        "MNIST": {"tf_serving": 22920.62122, "cpp_rpc": 21705.43172, "python": 19109.23245},
+        "CIFAR-10": {"tf_serving": 5293.848952, "cpp_rpc": 5297.627025, "python": 4448.834138},
+        "ImageNet": {"tf_serving": 56.84785542, "cpp_rpc": 53.39, "python": 46.78688182}
         }
     latencies = {
-        "MNIST": {"tf_serving": 43661.75, "cpp_rpc": 47821.65, "python": 45134.22536},
-        "CIFAR-10": {"tf_serving": 47298, "cpp_rpc": 47833.8615, "python": 56240.97845},
-        "ImageNet": {"tf_serving": 554837, "cpp_rpc": 595054.541, "python": 663444.01029}
+        "MNIST": {"tf_serving": 43647.37351, "cpp_rpc": 47147.81, "python": 53556.25},
+        "CIFAR-10": {"tf_serving": 47036.4517, "cpp_rpc": 48278.62, "python": 56495.01501},
+        "ImageNet": {"tf_serving": 561807.9518, "cpp_rpc": 598744.5983, "python": 683217.75}
         }
+    p99 = {
+        "MNIST": {"tf_serving": 44204, "cpp_rpc": 47899.781, "python": 55176.174},
+        "CIFAR-10": {"tf_serving": 51544, "cpp_rpc": 48976.383, "python": 60299.221},
+        "ImageNet": {"tf_serving": 574258, "cpp_rpc": 618484.563, "python": 714437.516}
+        }
+
     yranges = {
         "MNIST": [28000, 70.0],
-        "CIFAR-10": [6000, 70.0],
+        "CIFAR-10": [7000, 70.0],
         "ImageNet": [70, 750.0]
         }
+    titles = ["a) MNIST", "b) CIFAR-10", "c) ImageNet"]
+    for row, big_ax in enumerate(big_axes, start=1):
+        big_ax.set_title(titles[row-1], fontsize=8, fontweight='bold')
+        big_ax.tick_params(labelcolor=(1.,1.,1., 0.0), top='off', bottom='off', left='off', right='off')
+        # removes the white frame
+        big_ax._frameon = False
+    ax = [[fig.add_subplot(3, 2, 2 * i + j + 1) for j in range(2)] for i in range(3)]
+
     for (i,m) in enumerate(["MNIST", "CIFAR-10", "ImageNet"]):
-        ax_thru = ax[i,0]
-        ax_lat = ax[i,1]
+        ax_thru = ax[i][0]
+        ax_lat = ax[i][1]
         cur_thru = thruput[m]
         cur_lat = latencies[m]
+        cur_p99 = p99[m]
         for (offset,system) in enumerate(["tf_serving", "cpp_rpc", "python"]):
-            cur_rect = ax_thru.bar(width*offset, cur_thru[system],
+            err = (cur_p99[system] - cur_lat[system]) / 1000.0
+            cur_rect = ax_thru.bar(width*offset, cur_thru[system], 
                                    color=colors[offset], width=width, label=name_map[system])
-            utils.barchart_label(ax_thru, cur_rect, 6, rot=10)
+            utils.barchart_label(ax_thru, cur_rect, 6)
             cur_rect = ax_lat.bar(width*offset, cur_lat[system] / 1000.0,
-                                   color=colors[offset], width=width, label=name_map[system])
-            utils.barchart_label(ax_lat, cur_rect, 6, rot=10)
-        ax_thru.set_xticks(np.arange(1)*width*(num_bars + space) + width*(num_bars/2.0))
-        ax_thru.set_xticklabels([m], rotation=0, ha="center")
+                                  color=colors[offset], width=width, label=name_map[system],
+                                  yerr=[(0,), (err,)], ecolor='k', capsize=5)
+            utils.barchart_label(ax_lat, cur_rect, 6, hmult=0.90)
+        ax_thru.get_xaxis().set_visible(False)
         ax_thru.set_ylabel("Throughput (qps)")
         ax_thru.set_ylim(0, yranges[m][0])
-        ax_lat.set_xticks(np.arange(1)*width*(num_bars + space) + width*(num_bars/2.0))
-        ax_lat.set_xticklabels([m], rotation=0, ha="center")
+        ax_lat.get_xaxis().set_visible(False)
         ax_lat.set_ylabel("Mean Latency (ms)")
         ax_lat.set_ylim(0, yranges[m][1])
-    legend = ax[0,0].legend(frameon=True, bbox_to_anchor=(0.5, 1.05, 1.1, .602), loc=3,
-                mode="expand", borderaxespad=0.1, fontsize=8,)
-    plt.subplots_adjust(wspace=0.4, hspace=0.25, bottom=0.06, left=0.16, right=0.94, top=0.87)
+    legend = ax[0][0].legend(frameon=True, bbox_to_anchor=(-0.4, 1.2, 2.83, .102), loc=3,
+                ncol=3, mode="expand", borderaxespad=0.1, fontsize=8,)
+    plt.subplots_adjust(wspace=0.42, hspace=0.22, bottom=0.04, left=0.18, right=0.94, top=0.88)
+    # plt.show()
     fname = "%s/tf_serving_latency_thruput.pdf" % (fig_dir)
     plt.savefig(fname, bbox_inches='tight')
     print(fname)
@@ -167,7 +184,7 @@ def plot_latencies(figsize, colors):
 if __name__=='__main__':
     # results = load_results()
     # figsize = (5.2,1)
-    figsize = (4,6)
+    figsize = (4.5,6)
     colors = sns.color_palette("Set1", n_colors=8, desat=.5)
     plot_thrus_latency(figsize, colors)
     # plot_thrus(results, figsize, colors)
